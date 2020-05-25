926b61fdc7e4480a0a73808cf9de64ccee2666c6
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-25 23:08:12 +0200

    Create insert_seedlings.sql script

---
+++ database/database_scripts/insert_seedings.sql
@@ -0,0 +1,2 @@
+insert into seedling(name, id_company, progress, days_to_grow, id_nursery_garden, x, y)
+values ('test', 40, 10, 100, 1, 1,1), ('test 2', 40, 10, 100, 1, 2,1), ('test 2', 40, 10, 100, 1, 2,2);
