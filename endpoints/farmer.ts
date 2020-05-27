14388fb4f130a290ea77be787fe7c69ee4353ec0
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-27 23:25:49 +0200

    Update getGarden endpoint to return list of seedlings

--- endpoints/farmer.ts
+++ endpoints/farmer.ts
@@ -22,7 +22,7 @@ module.exports.getFarmerGardens = (request, response) => {
 
 module.exports.getFarmerGarden = (request, response) => {
   const id = parseInt(request.params.id)
-  
+  let result;
   db.getPool().query('select ng.id, ng.name, ng.place, ng.water, ' +
   'ng.temperature, CASE WHEN seedlings.occupied_slots is null THEN 0 ELSE seedlings.occupied_slots END as occupied_slots, ' +
   '(ng.height * ng.width - CASE WHEN seedlings.occupied_slots is null THEN 0 ELSE seedlings.occupied_slots END) as free_slots , ' +
@@ -36,6 +36,20 @@ module.exports.getFarmerGarden = (request, response) => {
     if (error) {
       return response.status(500).json(error);
     }
-    return response.status(200).json(results.rows)
-  })
+    result = {
+      ...results.rows[0],
+      seedlings : [],
+    }
+    db.getPool().query('select s.name, (progress * 1.0 / 100) as progress, x, y, u.fullname as producer ' +
+    'from seedling s ' +
+    'join users u on u.id = s.id_company ' +
+    'where id_nursery_garden =' + id, (error, results) => {
+      if (error) {
+        return response.status(500).json(error);
+      }
+      result.seedlings = results.rows;
+      return response.status(200).json(result)
+    })
+
+  });
 }
